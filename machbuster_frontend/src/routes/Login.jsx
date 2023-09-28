05fa7c9ece2a08cace1359facd69f702ed20e87c
Ken Bonnette <117300139+thisiskennyb@users.noreply.github.com>
2023-09-28 09:46:26 -0400

    login screen added

--- machbuster_frontend/src/routes/Login.jsx
+++ machbuster_frontend/src/routes/Login.jsx
@@ -1,3 +1,5 @@
+import SignIn from "../muiComponents/SignIn"
+
 
 export default function Login() {
     // useEffect(() => {
@@ -5,9 +7,7 @@ export default function Login() {
     //   }, [setToken]);
     return (
         <div className="route-text">
-        <h1>This is a login page</h1>
-        <p>lets do something nice with it</p>
-        <h3>This is the token</h3>
+        <SignIn />
         </div>
     )
 }
