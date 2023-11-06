d771dad200ab4c4941eac0a6e24c50f2f41cffb3
successPhil <129565748+successPhil@users.noreply.github.com>
2023-11-06 13:50:21 -0500

    containers running for prod, not able to get token, made some adustments to settings and will try a fresh image

--- backend/machbuster_db/settings.py
+++ backend/machbuster_db/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*0i%xk*vy4pwh0%^c%+$graavyd&023guce8o0oof4-_6g1@n)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
