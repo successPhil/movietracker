516ff7826d748b2042122894b475fde234a64d71
Ken Bonnette <117300139+thisiskennyb@users.noreply.github.com>
2023-11-06 12:20:56 -0500

    docker-compose file changes

--- run-compose-prod.sh
+++ run-compose-prod.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # The Dockerhub account where the images are stored
-export DOCKERHUB_UNAME=successphil
+export DOCKERHUB_UNAME=thisiskennyb
 
 # These environment variables come from command line arguments.
 # They are consumed by the docker-compose file.
